830e13f33f67256cbfbe1da99ec8b3823e305da5
norristown <jarvasity@gmail.com>
2023-03-15 19:19:58 -0400

    Second day

--- script.js
+++ script.js
@@ -30,3 +30,51 @@ const operate = function(a, b, op) {
     }
 }
 operate();
+
+//CALCULATOR
+const display = document.querySelector('.display');
+
+//CLEAR
+function clear() {
+    const clear = document.querySelector('.clear');
+    clear.addEventListener('click', () => {
+        display.textContent = "0";
+});
+};
+clear();
+
+//Number => display functions
+const one = document.querySelector('.one');
+one.addEventListener('click', () => {
+    if (display.textContent === '0') {
+        display.innerHTML = "";
+        display.textContent += '1';
+    } else {
+        display.textContent += "1";
+    }
+});
+
+const two = document.querySelector('.two');
+two.addEventListener('click', () => {
+    if (display.textContent === '0') {
+        display.innerHTML = "";
+        display.textContent += '2';
+    } else {
+        display.textContent += "2";
+    }
+});
+
+const three = document.querySelector('.three');
+three.addEventListener('click', () => {
+    if (display.textContent === '0') {
+        display.innerHTML = "";
+        display.textContent += '3';
+    } else {
+        display.textContent += "3";
+    }
+});
+
+const plus = document.querySelector('.plus');
+plus.addEventListener('click', () => { 
+    add(display.textContent, )
+});
